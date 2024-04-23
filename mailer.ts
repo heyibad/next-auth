647cc469d93ade953675a342793e18d05eab6704
Muhammad Ibad <ibad3572@gmail.com>
2024-04-23 09:25:51 +0000

    Refactor mailer module to use try-catch block for error handling

--- mailer.ts
+++ mailer.ts
@@ -7,21 +7,27 @@ interface mailerProps{
 }
 
 const mailer=async({email,emailType,userID}:mailerProps) =>{
-
-    const transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: false, // Use `true` for port 465, `false` for all other ports
-        auth: {
-          user: "maddison53@ethereal.email",
-          pass: "jn7jnAPss4f63QBp6D",
-        },
-      });
-      const options = {
-        from: 'ibad3572@gmail.com', // sender address
-        to: email, // list of receivers
-        subject: emailType === "VERIFY" ? "VERIFICATION":"FORGOT PASSWORD", // Subject line
-        html: "<b>Hello world?</b>", // html body
-      }
-      const mail = await transporter.sendMail(options);
+try {
+  
+      const transporter = nodemailer.createTransport({
+          host: "smtp.ethereal.email",
+          port: 587,
+          secure: false, // Use `true` for port 465, `false` for all other ports
+          auth: {
+            user: "maddison53@ethereal.email",
+            pass: "jn7jnAPss4f63QBp6D",
+          },
+        });
+        const mailOptions = {
+          from: 'ibad3572@gmail.com', // sender address
+          to: email, // list of receivers
+          subject: emailType === "VERIFY" ? "VERIFICATION":"FORGOT PASSWORD", // Subject line
+          html: "<b>Hello world?</b>", // html body
+        }
+        const mailResponse = await transporter.sendMail(mailOptions);
+        return mailResponse
+  
+} catch (error) {
+  console.log("Something happends while Sending Email")
+}
 } 
