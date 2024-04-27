35dd48f5ad2a3fde61e531c8f728e9e84013384a
Muhammad Ibad <ibad3572@gmail.com>
2024-04-27 13:47:53 +0500

    Refactor Navbar component, add it to Home page, and update layout

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -22,9 +22,9 @@ const Navbar = () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <Link href={'/'} className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Auth.</span>
-        </a>
+        </Link>
         <button
           onClick={toggleNavbar}
           data-collapse-toggle="navbar-default"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Navbar from "./components/Navbar";
 export default function Home() {
   return (
    <main>
-    <Navbar />
       <h1 className="mt-6 flex items-center justify-center text-3xl font-extrabold font-mono">Home</h1>
       <p className="flex items-center justify-center mt-2 text-lg font-mono">Welcome to the home page</p>
       <div className="flex justify-center my-4 ">  <Image src="https://img.freepik.com/premium-vector/2fa-authentication-password-secure-notice-login-verification-sms-with-push-code-message-shield-icon-smartphone-phone-laptop-computer-pc-flat_212005-139.jpg" alt="home" width={500} height={500} className="flex items-center justify-center rounded-lg" />
